bb46bc7d1098b7f1e8ae2890d60dde38a7f6b949
mencyliu <mx.mimi@qq.com>
2020-09-14 17:20:48 +0800

    add files

---
+++ 0. Hello World.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print('Hello world!')
+
+# Output ##############################################################
+# use ',' to combine strings
+
+print('The quick brown fox','jumps over','the lazy dog.')
+# The quick brown fox jumps over the lazy dog.
+
+print('100 + 200 =' , 100+200)
+# 100 + 200 = 300
+
+# Input >>>input（）##############################################################
+name = input('Tell me your name,plz.')
+print("Hi,",name)
